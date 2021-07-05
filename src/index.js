e30bb3c0fc0f9d6b9127a5a02bbe329f5ab2e6cc
Sunny Sharma <sunny@tripytrek.com>
2021-07-05 14:04:18 +0530

    removed scss file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-import * as styles from './stories/Button/Button.scss';
-export { styles };
 export * from './stories/Button/Button';
 export * from './stories/Input/Input';
 export * from './stories/Table/Table';
