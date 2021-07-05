4dd9b6bfbe43f0f6efe8ee223154d742245be5db
Sunny Sharma <sunny@tripytrek.com>
2021-07-05 14:15:15 +0530

    changes

--- src/stories/Input/Input.js
+++ src/stories/Input/Input.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import '../assets/styles/input.scss';
 
 export const Input = ({ type, placeholder, size, label, stretched, ...props }) => {
     const viewType = stretched ? 'sds-input--stretched' : '';
--- src/stories/Table/Table.js
+++ src/stories/Table/Table.js
@@ -1,6 +1,5 @@
 import React, {useState, useEffect} from 'react';
 import PropTypes from 'prop-types';
-import '../assets/styles/table.scss';
 
 export const Table = ({dataSource, columns, downloadable, scrollable, ...props }) => {
     const [filteredColumn, setFilteredColumn] = useState(columns);
