bea24acad7a6ad35bf05ea68e4e549d7d0233d5c
Sunny Sharma <sunny@tripytrek.com>
2021-07-12 18:02:04 +0530

    changes in tabs

--- src/stories/Tabs/Tabs.js
+++ src/stories/Tabs/Tabs.js
@@ -1,18 +1,34 @@
-import React, {useEffect} from 'react';
+import React, {Children, isValidElement, cloneElement, useState} from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 import { TabPane } from './TabPane';
 
-export const Tabs = ({ defaultActiveKey, onChange, ...props }) => {
+export const Tabs = ({ defaultActiveKey, onChange, children, ...props }) => {
 
-	useEffect(() => {
-		console.log("parent props",props);
-	})
+	const [activeTab, setActiveTab] = useState(defaultActiveKey);
 
+	const renderTabPane = () => {
+		Children.map(children, child => {
+			if (isValidElement(child) && activeTab === child.key) {
+			  return cloneElement(child, { isActive: defaultActiveKey ===  child.key})
+			}
+			return child;
+		});
+	  
+	}
+
+	const renderTabs = () => {
+		children.forEach((child) => {
+			return(
+				<div className="tabs" onClick={() => {setActiveTab(child.key)}} >{child.props.tab} {activeTab === child.key ? 'active' : ''}</div>
+			)
+		})
+	}
     return(
         <TabsContainer>
-			{props.children}
+			{renderTabs()}
+			{renderTabPane()}
         </TabsContainer>
     )
 
