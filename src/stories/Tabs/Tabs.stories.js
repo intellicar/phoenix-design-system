a3e8aad1557034bfd15fb210a5d90afad485a868
Sunny Sharma <sunny@tripytrek.com>
2021-07-14 17:33:57 +0530

    tabs changed

--- src/stories/Tabs/Tabs.stories.js
+++ src/stories/Tabs/Tabs.stories.js
@@ -1,5 +1,5 @@
 import React, {useEffect} from 'react';
-import { Title, Subtitle, Description, Primary, ArgsTable, Stories } from '@storybook/addon-essentials';
+// import { Title, Subtitle, Description, Primary, ArgsTable, Stories } from '@storybook/addon-essentials';
 
 
 import { Tabs } from './Tabs';
